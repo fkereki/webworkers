e9cbc8e6fe883d16a98094531a141baacd2b2017
Federico Kereki <fkereki@gmail.com>
2021-07-22 09:30:04 -0300

    Added sample output

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ const wc = webCall('workers/reverseString.worker.js');
 console.log(await wc('GENE AUTRY'));
 console.log(await wc('RED RYDER'));
 console.log(await wc('RAWHIDE KID'));
+
+/* Sample output at console
+
+index.js:7  SUCCESS #1 ENOL
+index.js:8  ERROR #1 Not a string true
+index.js:8  ERROR #1 Not a string 22
+index.js:7  SUCCESS #1 REGNAR
+index.js:8  ERROR #1 Not a string 9
+index.js:22 YRTUA ENEG
+index.js:23 REDYR DER
+index.js:24 DIK EDIHWAR
+index.js:3  FIBO 267914296
+index.js:16 SUCCESS #3 YDISSAC
+index.js:13 SUCCESS #2 GNOLAPOH
+
+*/
